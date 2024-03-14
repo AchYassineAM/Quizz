cbe350f26822004001125b02060eef2fffb52b6f
AchYassineAM <163415750+AchYassineAM@users.noreply.github.com>
2024-03-14 13:41:39 +0100

    Update quizz_app.py

--- quizz_app.py
+++ quizz_app.py
@@ -3,7 +3,7 @@ import pandas as pd
 import numpy as np
 
 # Charger le fichier Excel contenant les 99 questions et réponses
-file_path = "99Questions.xlsx"
+file_path = "AchYassineAM/Quizz/99Questions.xlsx"
 df = pd.read_excel(file_path)
 
 # Titre de l'application
